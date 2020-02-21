eebb78d77b2070514c62e59555b04cadd075a5e4
Kenneth Li <kql3@cornell.edu>
2020-02-20 22:47:45 -0500

    Add new LibGDX project

---
+++ desktop/src/com/glassboxgames/rubato/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.glassboxgames.rubato.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.glassboxgames.rubato.GDXRoot;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GDXRoot(), config);
+	}
+}
