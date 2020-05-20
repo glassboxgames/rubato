077015d889a618d31a0a96c2dfd0d52de77b9831
Kenneth Li <kql3@cornell.edu>
2020-05-20 22:53:29 +0000

    Fix wisp jittering

--- core/assets/pad.py
+++ core/assets/pad.py
@@ -3,7 +3,12 @@ import sys
 
 if __name__ == '__main__':
   filename = sys.argv[1]
+  top = 0
+  left = 2
+  bottom = 0
+  right = 0
   print("padding " + filename)
   im = Image.open(filename)
-  im2 = Image.new(im.mode, im.size[0], im.size[1] + 10, (0, 0, 0, 0))
-  im2.paste(im, (0, 10)).save(filename)
+  im2 = Image.new(im.mode, (im.size[0] + left + right, im.size[1] + top + bottom), (0, 0, 0, 0))
+  im2.paste(im, (left, top))
+  im2.save(filename)
